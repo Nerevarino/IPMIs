db58536f12753aa849f08b59ef6107e3cd84b7a8
Evgeniy Dronov <dronov-ep@yandex.ru>
2018-07-27 17:30:58 +0300

    Alpha_v2

--- generateIPMIs.php
+++ generateIPMIs.php
@@ -2,14 +2,18 @@
 
 include 'ip.php';
 
+file_put_contents("sourceIPMIs.txt", "");
+
 foreach(ipRange("172.16.0.2", "172.16.1.254") as $ip) {
     $command = "ipmitool -I lanplus -U ADMIN -P 21UfLv5ydYc212t -H $ip  mc info 1";
     echo $command . "\n";
     $result = "";
     $result = `$command`;
     if($result != "") {
-        file_put_contents("sourceIPMIs.txt", "ipaddress: $ip\n" . $result . "\n\n\n", FILE_APPEND);            
-    } else {}
+        file_put_contents("sourceIPMIs.txt", "ipaddress: $ip\n" . $result . "\n\n\n", FILE_APPEND);
+    } else {
+        file_put_contents("sourceIPMIs.txt", "ipaddress: $ip\nIPMI Version ???\nProduct ID ???" . "\n\n\n", FILE_APPEND);        
+    }
 }
 
 
